59bb95335dd8a07141507548f6c5ac1f2281ae95
Danilo Bürger <info@danilobuerger.de>
2016-02-07 22:29:47 +0100

    Added problem6

---
+++ problem6/problem6.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/danilobuerger/euler/numbers"
+)
+
+// https://projecteuler.net/problem=6
+//
+// The sum of the squares of the first ten natural numbers is,
+// 12 + 22 + ... + 102 = 385
+// The square of the sum of the first ten natural numbers is,
+// (1 + 2 + ... + 10)2 = 552 = 3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+func main() {
+	n := 100
+	sum := numbers.SumOfNumbers(n)
+	sum = sum*sum - numbers.SumOfSquares(n)
+
+	log.Println(sum)
+}
